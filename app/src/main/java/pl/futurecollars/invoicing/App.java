97570e6e0ad2ff0e95197aa29b33d5403df16e12
Marek <m@w.pl>
2023-02-22 20:46:53 +0100

    Lesson3

--- app/src/main/java/pl/futurecollars/invoicing/App.java
+++ app/src/main/java/pl/futurecollars/invoicing/App.java
@@ -19,9 +19,9 @@ public class App {
     InvoiceService service = new InvoiceService(db);
 
     Company buyer = new Company("5213861303", "ul. Bukowińska 24d/7 02-703 Warszawa, Polska", "iCode Trust Sp. z o.o");
-    Company seller = new Company("123-123-11-11", "11-111 Kraków, Testowa 123", "Marek Wiśniewski");
+    Company seller = new Company("552-168-66-00", "32-005 Niepolomice, Nagietkowa 19", "Piotr Kolacz Development");
 
-    List<InvoiceEntry> products = List.of(new InvoiceEntry("Programming course", BigDecimal.valueOf(10000), BigDecimal.valueOf(2300), Vat.VAT_23));
+    List<InvoiceEntry> products = List.of(new InvoiceEntry("Programming course", 1, BigDecimal.valueOf(10000), BigDecimal.valueOf(2300), Vat.VAT_23));
 
     Invoice invoice = new Invoice(LocalDate.now(), buyer, seller, products);
 
